b958a97f376f120a36a0fd46c1a7b85ca930e045
AmirMohammad Hosseini Nasab <awmirhn@gmail.com>
2022-02-05 00:39:42 +0330

    Add comparable hash for Timestamp

--- Database/Field/Field.cpp
+++ Database/Field/Field.cpp
@@ -7,6 +7,17 @@
 
 int Field::getHash() {
     if (type == Type::Integer) return stoi(data);
+    if (type == Type::Timestamp) {
+        smatch matches;
+        regex timeRgx("^(\\d{4})\\/(0?[1-9]|1[012])\\/(0?[1-9]|[12][0-9]|3[01])$");
+        if (regex_search(data, matches, timeRgx)) {
+            int year = stoi(matches[1].str());
+            int month = stoi(matches[2].str());
+            int day = stoi(matches[3].str());
+            return 372 * year + 31 * month + day;
+        }
+        throw invalid_argument("Invalid Date.");
+    }
     int h = 0;
     for (int i = 0; i < (int)(data.size()); i++) h = (1LL * h * HASH_BASE % HASH_MOD + (int)(data[i])) % HASH_MOD;
     return h;
